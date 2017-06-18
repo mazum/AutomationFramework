8518ec6f29320e3b6dd5125154c7a7166622c5f0
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-06-18 21:42:02 +1000

    CheckBox Webelement extension.

---
+++ Framework/WebElements/CheckBox.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace Framework.WebElements
+{
+    public class Checkbox
+    {
+        private IWebElement checkbox;
+
+        public Checkbox(IWebElement checkbox)
+        {
+            this.checkbox = checkbox;
+        }
+
+        public void Select(bool value)
+        {
+            if (checkbox.Selected != value)
+                checkbox.Click();
+        }
+
+        public bool IsSelected()
+        {
+            return checkbox.Selected;
+        }
+    }
+}
