aaa95ea9e96a506da7307a4859d3800fa918ef96
Manosi Mitra <manosimitra28@gmail.com>
2017-06-23 23:34:25 +1000

    Add method public static IList<string> GetTextFromElements(this IList<IWebElement> elementList).

--- Framework/Extensions/WebElementExtensions.cs
+++ Framework/Extensions/WebElementExtensions.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Internal;
+using OpenQA.Selenium.Support.UI;
 
 namespace Framework.Extensions
 {
-    class WebElementExtensions
+    public static class WebElementExtensions
     {
+        public static IList<string> GetTextFromElements(this IList<IWebElement> elementList)
+        {
+            var textList = new List<string>();
+            foreach (var element in elementList)
+                if (element.Displayed)
+                    textList.Add(element.Text.Trim());
+            return textList;
+        }
     }
 }
