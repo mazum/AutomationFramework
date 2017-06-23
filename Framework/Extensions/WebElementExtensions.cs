7650afc4be75e6b6eaf20d6e70fd3b0315e7bbb8
Manosi Mitra <manosimitra28@gmail.com>
2017-06-23 23:57:01 +1000

    Added method GetMatchingTextFromElements

--- Framework/Extensions/WebElementExtensions.cs
+++ Framework/Extensions/WebElementExtensions.cs
@@ -2,7 +2,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Internal;
-using OpenQA.Selenium.Support.UI;
+//using OpenQA.Selenium.Support.UI;
 
 namespace Framework.Extensions
 {
@@ -14,6 +14,14 @@ namespace Framework.Extensions
             foreach (var element in elementList)
                 if (element.Displayed)
                     textList.Add(element.Text.Trim());
+            return textList;
+        }
+
+        public static IList<string> GetMatchingTextFromElements(this IList<IWebElement> elementList, string regExPattern)
+        {
+            var textList = new List<string>();
+            foreach (var element in elementList)
+                textList.Add(element.Text.GetRegExMatch(regExPattern).Trim());
             return textList;
         }
     }
