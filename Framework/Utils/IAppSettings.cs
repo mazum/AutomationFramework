ab7f2f232e5daa1e91dc4b3ee18f9322dcf1659f
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-06-26 21:44:07 +1000

    IAppSettings.cs added to Utils.

---
+++ Framework/Utils/IAppSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace Framework.Utils
+{
+    public interface IAppSettings
+    {
+        string Browser { get; set; }
+        string BrowserUrl { get; set; }
+        int ImplicitWaitTimeoutSeconds { get; set; }
+        int PageLoadTimeoutSeconds { get; set; }
+        int PageTitleWaitSeconds { get; set; }
+        string GridUrl { get; set; }
+        bool EnableCookieInfoLogging { get; set; }
+        string ScreenshotFolder { get; set; }
+        bool IgnoreProductionIssues { get; set; }
+    }
+}
