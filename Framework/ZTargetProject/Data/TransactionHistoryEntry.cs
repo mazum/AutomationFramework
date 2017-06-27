f11e59655af33dc75d5ed9e72489e01c2dceb37a
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-06-27 21:54:15 +1000

    Fixed dependencies of CsvHelper.

---
+++ Framework/ZTargetProject/Data/TransactionHistoryEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Constraints;
+
+namespace Framework.ZTargetProject.Data
+{
+    public class TransactionHistoryEntry
+    {
+        public string Date { get; set; }
+        public string TransactionType { get; set; }
+        public string Description { get; set; }
+        public string Component { get; set; }
+        public string Amount { get; set; }
+        public string Balance { get; set; }
+    }
+}
