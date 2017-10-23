10400be4b2b5c8c3d88e0b366339882f6c540557
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-10-23 23:19:41 +1100

    ScenarioContextData added.

---
+++ Framework/Utils/IScenarioContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Framework.ZTargetProject.Pages;
+//using USM.Simple.API.MemberChannel;
+
+namespace Framework.Utils
+{
+    public interface IScenarioContext
+    {
+        BasePage CurrentPage { get; set; }
+        BaseMemberPage CurrentMemberPage { get; set; }
+        string Username { get; set; }
+        string Password { get; set; }
+        string AccountNumber { get; set; }
+        string AccountType { get; set; }
+        string AccountBaseType { get; set; }
+        string AccountSubType { get; set; }
+        string MemberNumber { get; set; }
+        bool HasDemoVideoDisplayDate { get; set; }
+        DateTime DemoVideoDisplayDate { get; set; }
+        bool IsSpouseAccount { get; set; }
+        string PersonalEmailAddress { get; set; }
+        string PersonalMobileNumber { get; set; }
+        MailItem LastEmail { get; set; }
+        bool HasMultipleAccounts { get; set; }
+        bool IsWEHI { get; set; }
+        int ActiveSuper { get; set; }
+        int ActivePension { get; set; }
+        int ActiveIndexedPension { get; set; }
+        bool HasExitedAccounts { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        DateTime DateOfBirth { get; set; }
+        //List<AttributeData> SmvAccountsAttributes { get; set; }
+    }
+}
